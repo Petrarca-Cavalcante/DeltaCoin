35c19b249c83e409338c24ac8e78ef6584186120
Petrarca ACS Brasil <pedropetrarcar@gmail.com>
2025-08-05 13:50:49 -0300

    feat: find many html elements method.

--- app/rpa/driver.py
+++ app/rpa/driver.py
@@ -15,4 +15,42 @@ class Webdriver:
         self.log = Log("Webdriver", print_else=True)
 
     # ---------------------------------------------------------------------------
+    def find_many(
+        self,
+        xpath: str,
+        timeout: int = 10,
+        bkg_c: str = "orange",
+        show_exc: bool = True,
+    ):
+        """Find and return one HTML element based in the xpath given."""
+        try:
 
+            self.log.write(f"Finding clickable: {xpath}", level="FIND_C")
+
+            html_elements = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_all_elements_located((By.XPATH, xpath))
+            )
+
+            self.log.write(f"Clickable found: {xpath}", level="FOUND_C")
+            if html_elements:
+                for el in html_elements:
+                    self.driver.execute_script(
+                        f"""
+                        arguments[0].style.backgroundColor = '{bkg_c}';
+                        arguments[0].style.color = 'white';
+                        arguments[0].style.fontWeight = 'bold';
+                        """,
+                        el,
+                    )
+                return html_elements
+
+        except Exception as e:
+            if show_exc:
+                ...
+                self.log.write(f"Error finding clickable XPATH: {xpath}", level="ERROR")
+                self.log.write(f"Error finding clickable ERROR: {e}", level="ERROR")
+            if not show_exc:
+                ...
+                self.log.write(
+                f"Clicakble not found, but it was expected to: {xpath}", level="S_ERROR"
+                )
