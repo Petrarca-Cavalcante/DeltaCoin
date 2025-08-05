49e37fcda66fc1241d6e0181b915a5d81eef2c46
Petrarca ACS Brasil <pedropetrarcar@gmail.com>
2025-08-05 15:02:56 -0300

    feat: RPA method added.

--- app/index.py
+++ app/index.py
@@ -12,7 +12,6 @@ class App:
     def __init__(self) -> None:
         self.log = Log("App", print_else=True)
 
-    
     def from_api(self):
         self.log.write("Running app by api method", f_log=True, level="App.from_api")
         app = DeltaCoin(os.getenv("API_PDF_PATH"))
@@ -26,4 +25,18 @@ class App:
         app.mail(subject="Atualização no preço do Dollar / Euro",
                  body="Segue último valor capturado pela automação",
                  destiny=os.getenv("EMAILS"))
+
+
+    def from_rpa(self):
+        self.log.write("Running app by rpa method", f_log=True, level="App.from_rpa")
+        app = DeltaCoin(os.getenv("RPA_PDF_PATH"))
+        request = app.rpa
+        self.log.write("Request done", level="App.from_rpa")
         
+        self.log.write("Writing into pdf.", level="App.from_rpa")
+        app.to_pdf(request) 
+        self.log.write("Done writing into pdf.", level="App.from_rpa")
+        
+        app.mail(subject="Atualização no preço do Dollar / Euro",
+                 body="Segue último valor capturado pela automação",
+                 destiny=os.getenv("EMAILS"))
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from app.index import App
 
 def main():
     app = App()
-    # app.from_api()
+    app.from_api()
+    app.from_rpa()
     
     
 main()
