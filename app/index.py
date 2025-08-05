a2b8f4d812106895dd77dcb97ae9e60ee8158844
Petrarca ACS Brasil <pedropetrarcar@gmail.com>
2025-08-05 14:46:38 -0300

    feat: app class with - from api features added.

--- app/index.py
+++ app/index.py
@@ -11,3 +11,19 @@ class App:
     
     def __init__(self) -> None:
         self.log = Log("App", print_else=True)
+
+    
+    def from_api(self):
+        self.log.write("Running app by api method", f_log=True, level="App.from_api")
+        app = DeltaCoin(os.getenv("API_PDF_PATH"))
+        request = app.api
+        self.log.write("Request done", level="App.from_api")
+        
+        self.log.write("Writing into pdf.", level="App.from_api")
+        app.to_pdf(request) 
+        self.log.write("Done writing into pdf.", level="App.from_api")
+        
+        app.mail(subject="Atualização no preço do Dollar / Euro",
+                 body="Segue último valor capturado pela automação",
+                 destiny=os.getenv("EMAILS"))
+        
