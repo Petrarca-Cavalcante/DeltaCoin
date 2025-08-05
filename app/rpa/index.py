d3a6e8169fb26469047921ebf1c72be3575c403e
Petrarca ACS Brasil <pedropetrarcar@gmail.com>
2025-08-05 13:42:05 -0300

    feat: Requesting price by RPA.

--- app/rpa/index.py
+++ app/rpa/index.py
@@ -30,3 +30,36 @@ class DeltaCoin:
             return request
         except Exception:
             raise Exception("Error at requesting API-s")
+
+    @property
+    def rpa(self):
+        cd = Webdriver()
+        wd = cd.driver
+        wd.get("https://www.bcb.gov.br")
+
+        # ENCONTRA TABELAS DE COTAÇÃO
+        tables = cd.find_many("//div[@class='componente cotacao']//table") or []
+        prices = []
+
+        for table in tables:
+            # CAPTURA CABEÇALHOS E CONTEÚDOS
+            headers = cd.find_in(table, "./thead/tr/th")
+            tds = cd.find_in(table, "./tbody/tr/td")
+            #
+            # CASO NÃO SEJA ENCONTRADO, ELE É "PULADO"
+            if not headers or not tds:
+                # > Adicionar log e meio de "recuperação" da tarefa perdida
+                continue
+            #
+            coin = headers[0].text
+            if coin not in prices:
+                prices.append({
+                    "Moeda": coin,
+                    "Compra":tds[4].text,
+                    "Venda": tds[5].text,
+                    "Atualizado_em": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                })
+            #
+        cd.kill()
+        return prices
+
